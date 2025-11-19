cd671f96c26fba4de16991de05e774bc0828ab8c
dmccowan <mccowa206@morrisville.edu>
2025-11-19 04:15:06 -0500

    Player Controller and floors

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using StarterAssets;
+
+public class Interactor : MonoBehaviour
+{
+    StarterAssetsInputs starterAssetsInputs;
+    FirstPersonController firstPersonController;
+    void Awake()
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+        firstPersonController = GetComponent<FirstPersonController>();
+    }
+    void Update()
+    {
+        HandleInteract();
+    }
+    void HandleInteract()
+    {
+        if (!starterAssetsInputs.interact) return;
+
+        starterAssetsInputs.InteractInput(false);
+
+        Debug.Log($"Passed if");
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, firstPersonController.interactRange))
+        {
+            Debug.Log($"Touched {hit.collider}");
+        }
+    }
+}
